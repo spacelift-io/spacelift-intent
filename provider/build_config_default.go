f09edb85555ad359fd7bfa371747c52a71ba0f19
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2025-09-19 09:47:41 +0200

    Use build tags for legacy plugin (#6)

---
+++ provider/build_config_default.go
@@ -0,0 +1,6 @@
+//go:build !legacy_plugin
+
+package provider
+
+// useLegacyProvider indicates whether to use the legacy hashicorp/go-plugin implementation
+const useLegacyProvider = false
--- provider/opentofu_adapter.go
+++ provider/opentofu_adapter.go
@@ -1,3 +1,5 @@
+//go:build !legacy_plugin
+
 package provider
 
 import (
